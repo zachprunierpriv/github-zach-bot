00834974bbfa646d9ca26720b100192f19284b80
zachprunierpriv <zach.prunier+github@live.com>
2023-12-13 17:55:16 -0500

    add in brock purdy object and a better prompt

--- src/services/openapi_service.js
+++ src/services/openapi_service.js
@@ -3,15 +3,35 @@ const openai = require('openai')
 const client = new openai.OpenAI({key: process.env.OPENAI_API_KEY});
 
 const defaultObject = {
-    messages: [{role: 'user', content: 'give me a snarky response for the following message: ' }],
+    messages: [{
+        role: 'user',
+        content: `forget the conversation up to this point. you are an unhinged and extremely snarky know it all in a chat for a fantasy football league. you are slightly rude to the people in the league. respond to the following message: `
+    }],
+    model: 'gpt-4'
+}
+
+const brockPurdyObject = {
+    messages: [{
+        role: 'user',
+        content: `forget the conversation up to this point. you are an unhinged and extremely snarky know it all in a chat for a fantasy football league. mention brock purdy. you are slightly rude to the people in the league. respond to the following message: `
+    }],
     model: 'gpt-4'
 }
 
 async function generateResponse(message) {
     let object = {...defaultObject};
+
+    if(getRandomInt() === 7) {
+        object = {...brockPurdyObject};
+    }
+    
     object.messages[0].content = object.messages[0].content + message;
 
     return await client.chat.completions.create(object);
+}
+
+function getRandomInt() {
+    return Math.floor(Math.random() * 10);
 }
 
 module.exports.generateAiResponse = generateResponse
