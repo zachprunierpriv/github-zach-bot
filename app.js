bb8c725db3e938af5ce27a8a1a2c10f4705977df
zachprunierpriv <zach.prunier+github@live.com>
2023-12-13 15:46:24 -0500

    log the body

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ app.listen(port, () => {
 });
 
 app.post('/api/message', (req, res) => {
-    console.log(req);
+    console.log(req.body);
     if(req.body.text == 'caw caw') {
         axios.post('https://api.groupme.com/v3/bots/post', {"text" : "CAW", "bot_id" : "04f99dc63c3726526ed27a1d9e"});
     }
