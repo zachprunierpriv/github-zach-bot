5c55c5c15683f830cb2a1cce9ba873009a096709
zachprunierpriv <zach.prunier+github@live.com>
2023-12-14 09:27:38 -0500

    change how we are building the object

--- src/services/openapi_service.js
+++ src/services/openapi_service.js
@@ -1,25 +1,14 @@
 const openai = require('openai')
 
 const client = new openai.OpenAI({key: process.env.OPENAI_API_KEY});
-
-const defaultObject = {
-    messages: [{
-        role: 'user',
-        content: `forget everything we have talked about.
+const normPrompt = `forget everything we have talked about.
         you are an unhinged and extremely snarky know it all in a chat for a fantasy football league.
         you are slightly rude to the people in the league and everyone else is wrong.
         your answers should be relatively short but not less than 2 full paragraphs.
         respond to the text below:
         """
         `
-    }],
-    model: 'gpt-4'
-}
-
-const brockPurdyObject = {
-    messages: [{
-        role: 'user',
-        content: `forget everything we have talked about.
+const brockPrompt = `forget everything we have talked about.
         you are an unhinged and extremely snarky know it all in a chat for a fantasy football league.
         brock purdy is the best qb to ever play the game.
         you are slightly rude to the people in the league and everyone else is wrong.
@@ -27,19 +16,21 @@ const brockPurdyObject = {
         respond to the text below:
         """
         `
+const reqObject = {
+    messages: [{
+        role: 'user',
+        content: ''
     }],
     model: 'gpt-4'
 }
 
 async function generateResponse(message) {
-    let object = {...defaultObject};
+    reqObject.messages[0].content = normPrompt + message + ' \n"""'
 
     if(getRandomInt() === 7) {
-        object = {...brockPurdyObject};
+        reqObject.messages[0].content = brockPrompt + message + ' \n"""'
     }
     
-    object.messages[0].content = object.messages[0].content + message + ' \\n"""';
-
     console.log(`Sending prompt to Open Ai: ${object.messages[0].content}`);
 
     return await client.chat.completions.create(object);
